fa3efa942eab98bc0ec3073789ec1187794bfa47
Erick Hernandez <takashi.onimaru@gmail.com>
2025-01-15 15:42:56 -0400

    "Update version number in pagadito-gateway.php from 3.0.5 to 3.0.6"

--- pagadito-gateway.php
+++ pagadito-gateway.php
@@ -5,7 +5,7 @@
  * Description: Custom payment gateway plugin for the Pagadito platform.
  * Author: Erick Hernandez
  * Author URI: http://erdesarrollo.com.ve
- * Version: 3.0.5
+ * Version: 3.0.6
  */
 
 require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
